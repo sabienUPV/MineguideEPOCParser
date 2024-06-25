a33aca97c20223008af760207b19ed82c58f705c
Alejandro Gómez <algono@inf.upv.es>
2024-06-25 12:38:53 +0200

    Unknown group (UNK) if medication group is not found

--- MineguideEPOCParser.Core/MedicationMapperGroupingParser.cs
+++ MineguideEPOCParser.Core/MedicationMapperGroupingParser.cs
@@ -5,6 +5,8 @@ namespace MineguideEPOCParser.Core
 {
     public class MedicationMapperGroupingParser : MedicationParser<MedicationMapperGroupingParserConfiguration>
     {
+        public const string UnknownGroup = "UNK";
+
         /// <summary>
         /// Original dictionary loaded from the JSON file.
         /// (Key: Group name, Value: Array of medication names)
@@ -49,15 +51,9 @@ namespace MineguideEPOCParser.Core
                 // Get the medication name
                 var t = row[inputColumnIndex];
 
-                // Get the group of the medication
-                var group = NameToGroup!.GetValueOrDefault(t);
-
-                // If the medication is not in any group, return the row as is and continue
-                if (group == null)
-                {
-                    yield return row;
-                    continue;
-                }
+                // Get the group of the medication.
+                // If the medication is not in a group, use "UNK" as the group name
+                var group = NameToGroup!.GetValueOrDefault(t, UnknownGroup);
 
                 // If the medication is in a group, replace the medication name with the group name
                 var newRow = row.Select((value, index) => index == inputColumnIndex ? group : value).ToArray();
