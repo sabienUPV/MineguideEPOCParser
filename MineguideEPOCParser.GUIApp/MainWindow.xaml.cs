2c085613fc8363735fa54d5aacf3ce27da75ed28
algono <algono@inf.upv.es>
2025-07-18 14:38:29 +0200

    FIX/REFACTOR MainWindow use menus instead of tabs to dynamically create controls
    Not sure if TabControl was lazy loading the controls or not, but this should make sure other controls are not being loaded at the same time

--- MineguideEPOCParser.GUIApp/MainWindow.xaml.cs
+++ MineguideEPOCParser.GUIApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace MineguideEPOCParser.GUIApp
 {
@@ -7,14 +8,60 @@ namespace MineguideEPOCParser.GUIApp
     /// </summary>
     public partial class MainWindow : Window
 	{
-		public MainWindow()
+        private ContentControl? _currentControl;
+
+        private (string Header, Func<ContentControl> Factory)[] _controlFactories = new (string, Func<ContentControl>)[]
+        {
+            ("Medication Parser", () => new MedicationParserControl()),
+            ("Medication Mapper and Grouping to Group Mapper", () => new MedicationMapperGroupingControl()),
+            ("Medication Grouping To Mapper", () => new MedicationGroupingToMapperControl()),
+            ("File Encoding Converter", () => new FileEncodingConverterControl()),
+            ("Measurements Parser", () => new MeasurementsParserControl()),
+            ("Medication Manual Validator", () => new MedicationManualValidatorControl()),
+            ("Random Sampler Parser", () => new RandomSamplerParserControl())
+        };
+
+        public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Initialize the main content control with the first control
+            _currentControl = _controlFactories[0].Factory();
+            MainContentControl.Content = _currentControl;
+            // Populate the menu with control options
+            foreach (var (header, factory) in _controlFactories)
+            {
+                var menuItem = new MenuItem { Header = header };
+                menuItem.Click += async (s, args) => await ChangeContentControl(factory());
+                MainMenu.Items.Add(menuItem);
+            }
+        }
+
+        private async Task ChangeContentControl(ContentControl newControl)
+        {
+            await DisposeCurrentControl();
+            _currentControl = newControl;
+            MainContentControl.Content = _currentControl;
+        }
+
         private async void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            await MedicationParserCtrl.DisposeAsync();
+            await DisposeCurrentControl();
+        }
+
+        private async Task DisposeCurrentControl()
+        {
+            if (_currentControl is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+            else if (_currentControl is IAsyncDisposable asyncDisposable)
+            {
+                await asyncDisposable.DisposeAsync();
+            }
         }
     }
 }
