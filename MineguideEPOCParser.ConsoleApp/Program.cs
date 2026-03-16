59716dcb67e10aad6ffbb2704aee67ed80ec18b9
algono <algono@inf.upv.es>
2026-03-16 13:04:36 +0100

    Complete ConsoleApp support for medication parsing with Cocona

--- MineguideEPOCParser.ConsoleApp/Program.cs
+++ MineguideEPOCParser.ConsoleApp/Program.cs
@@ -1,47 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using Cocona;
 using MineguideEPOCParser.Core;
 using Serilog;
 
-using CancellationTokenSource cts = new();
-
+// 1. Keep your Serilog setup exactly as is
 await using var log = new LoggerConfiguration()
-	.MinimumLevel.Verbose() // Log everything by default
-	.WriteTo.Console()
-	.CreateLogger();
+    .MinimumLevel.Verbose() // Log everything by default
+    .WriteTo.Console()
+    .CreateLogger();
 
-// TEST JUAN
-//var testConfiguration = TestConfigurations.JuanConfig();
+// 2. Initialize the Cocona App
+var app = CoconaApp.Create();
 
-// TEST ALEJANDRO
-var testConfiguration = TestConfigurations.AlejandroConfig();
-
-log.Information("Application started.");
-Console.WriteLine("Press the ENTER key to cancel...\n");
-
-Task cancelTask = Task.Run(() =>
+// 3. Define your main command. Cocona maps these parameters to CLI arguments automatically!
+app.AddCommand(async (
+    [Option('i', Description = "The path to the input file to parse.")] string inputFile,
+    [Option('o', Description = "The path where the output should be saved.")] string outputFile,
+    [Option('c', Description = "Culture code for parsing (e.g., 'en-US'). Set to your computer's culture by default")] string? culture,
+    CancellationToken ct) =>
 {
-	while (Console.ReadKey().Key != ConsoleKey.Enter)
-	{
-		Console.WriteLine("Press ENTER key to cancel the operation...");
-	}
+    log.Information("Application started. Press Ctrl+C to cancel...\n");
 
-	log.Information("Cancelling operation...");
-	cts.Cancel();
+    try
+    {
+        // 4. Map the CLI arguments to your configuration object
+        // (Replace "YourConfigurationClass" with whatever type TestConfigurations returns)
+        var runConfiguration = new MedicationExtractingParserConfiguration
+        {
+            InputFile = inputFile,
+            OutputFile = outputFile,
+            CultureName = culture ?? System.Globalization.CultureInfo.CurrentCulture.Name
+        };
+
+        var medicationParser = new MedicationExtractingParser()
+        {
+            Configuration = runConfiguration,
+            Logger = log
+        };
+
+        // 5. Pass Cocona's built-in cancellation token (triggered by Ctrl+C)
+        await medicationParser.ParseData(ct);
+        log.Information("ParseMedication completed");
+    }
+    catch (OperationCanceledException)
+    {
+        log.Information("ParseMedication has been cancelled by the user.");
+    }
+    catch (Exception ex)
+    {
+        log.Error(ex, "An error occurred during parsing.");
+    }
 });
 
-try
-{
-	var medicationParser = new MedicationExtractingParser()
-	{
-		Configuration = testConfiguration,
-		Logger = log
-	};
-
-	await medicationParser.ParseData(cts.Token);
-	log.Information("ParseMedication completed");
-}
-catch (OperationCanceledException)
-{
-    log.Information("ParseMedication has been cancelled.");
-}
+// 6. Run the application
+await app.RunAsync();
